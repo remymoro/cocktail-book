1ce94fe5bc3a9965ecd865943877a16e4257493a
moro <moro.remy879@gmail.com>
2025-01-04 15:15:28 +0100

    ✨ Added CocktailDetailsComponent and integrated with CocktailsComponent

--- src/app/components/cocktails/cocktails.component.ts
+++ src/app/components/cocktails/cocktails.component.ts
@@ -6,8 +6,8 @@ import { CocktailDetailsComponent } from './components/cocktail-details.componen
   selector: 'app-cocktails',
   imports: [CocktailsListComponent, CocktailDetailsComponent],
   template: `
-    <app-cocktails-list class="flex-auto card" />
-    <app-cocktail-details class="flex-auto card" />
+    <app-cocktails-list class="w-half card" />
+    <app-cocktail-details class="w-half card" />
   `,
   styles: `
     :host {
