b89127fc83695b77c2cf77ac576a4d47afb14dfd
moro <moro.remy879@gmail.com>
2025-01-11 18:46:33 +0100

    Setup admin panel architecture

---
+++ src/app/views/admin/components/admin-navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  template: `<ul class="flex flex-col">
+    <a class="my-2" routerLink="cocktails" routerLinkActive="active-link"
+      >Cocktails</a
+    >
+    <a class="my-2" routerLink="users" routerLinkActive="active-link">Users</a>
+  </ul>`,
+  host: { class: 'card flex flex-col p-12' },
+  styles: `:host { width: 200px; }`,
+})
+export class AdminNavbarComponent {}
