d244c4283b05e8ea83fd117e6d73d0bdc8a6d9bd
moro <moro.remy879@gmail.com>
2025-01-11 17:55:45 +0100

    Add functionality to display the list of ingredients

--- src/app/views/cart/components/cart-ingredients-list.component.ts
+++ src/app/views/cart/components/cart-ingredients-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input } from '@angular/core';
+import { Component, computed, effect, input } from '@angular/core';
 
 @Component({
   selector: 'app-cart-ingredients-list',
@@ -6,7 +6,13 @@ import { Component, effect, input } from '@angular/core';
   template: `
     <h2>Liste des ingrédients</h2>
     <ul>
-      <!----@for()---!-->
+      @for (ingredient of ingredientDisplays(); track $index) {
+      <li class="my-2">
+        {{ ingredient[0] }} : <strong>{{ ingredient[1] }}</strong>
+      </li>
+      }@empty {
+      <h1>Aucun ingrédient n'as été trouvé pour le moment</h1>
+      }
     </ul>
   `,
   styles: `:host{
@@ -14,10 +20,18 @@ import { Component, effect, input } from '@angular/core';
   }`,
 })
 export class CartIngredientsListComponent {
-  ingredients = input<string[]>();
-  constructor() {
-    effect(() => {
-      console.log(this.ingredients());
-    });
-  }
+  ingredients = input<string[]>([]);
+  ingredientDisplays = computed(() =>
+    Object.entries(
+      this.ingredients().reduce((acc, i) => {
+        if (acc[i]) {
+          acc[i]++;
+        } else {
+          acc[i] = 1;
+        }
+
+        return acc;
+      }, {} as { [s: string]: number })
+    )
+  );
 }
